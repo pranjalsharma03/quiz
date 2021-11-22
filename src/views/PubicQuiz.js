7fddda69e0cd4adeda06d8c826027228047529fc
sagarsinghal2 <sagarsinghal222@gmail.com>
2021-11-22 15:53:14 +0530

    code jobma

--- src/views/PubicQuiz.js
+++ src/views/PubicQuiz.js
@@ -198,7 +198,7 @@ class PublicQuiz extends React.Component {
           </Row>
           <Row>
             <Col md="3" className="text-right d-none d-md-block ">
-              <img src={logo} alt="Quiz Yaar" width={100} height={100} />
+              <img src={logo} alt="Code Jobma" width={100} height={100} />
             </Col>
             <Col md="9">
               <h3 className="text-light text-center text-lg-left my-auto">
@@ -258,7 +258,7 @@ class PublicQuiz extends React.Component {
                 <CardBody>
                   <h4 className="text-left text-justify text-light mx-4 my-auto">
                     "An investment in knowledge pays the best interest".
-                    Quizyaar is a free platform where you just need to invest
+                    Code Jobma is a free platform where you just need to invest
                     your learnings and explore your knowledge.
                   </h4>
                 </CardBody>
